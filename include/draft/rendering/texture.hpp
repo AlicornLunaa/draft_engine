903e1b80ae38254babdd334cdcce95f23305cfeb
AlicornLunaa <alicornlunaa@gmail.com>
2024-04-14 15:11:46 -0500

    Refactored texture and fixed hot reloading textures

--- include/draft/rendering/texture.hpp
+++ include/draft/rendering/texture.hpp
@@ -1,33 +1,41 @@
 #pragma once
 
-#include <string>
+#include <filesystem>
 
 #include "draft/math/vector2.hpp"
+#include "draft/util/file_handle.hpp"
 
 namespace Draft {
     class Texture {
-    private:
-        // Variables
-        unsigned int texId;
-        std::string path;
-        Vector2i size;
-        int nrChannels;
-        bool loaded = false;
-        bool reloadable = true;
-
-        // Private functions
-        void load_texture(const std::string& texturePath);
-
     public:
         // Enums
         enum Wrap { REPEAT = 0x2901, MIRRORED_REPEAT = 0x8370, CLAMP_TO_EDGE = 0x812F, CLAMP_TO_BORDER = 0x812D };
         enum Filter { NEAREST = 0x2600, LINEAR = 0x2601 };
+        enum ColorSpace { RGB = 0x1907, RGBA = 0x1908 };
+        
+    private:
+        // Variables
+        const bool reloadable;
+        bool loaded = false;
 
+        FileHandle handle;
+        Vector2i size;
+        int nrChannels;
+
+        unsigned int texId;
+
+        // Private functions
+        void generate_opengl(Wrap wrapping);
+        void load_texture(const unsigned char* bytes, size_t length, int colorSpace = RGB);
+        void cleanup();
+
+    public:
         // Constructors
         Texture(Wrap wrapping = REPEAT);
-        Texture(const std::string& texturePath, Wrap wrapping = REPEAT);
-        Texture(const unsigned char* data, int width, int height, int channels, Wrap wrapping = REPEAT);
         Texture(const char* start, const char* end, Wrap wrapping = REPEAT);
+        Texture(const std::filesystem::path& texturePath, Wrap wrapping = REPEAT);
+        Texture(const FileHandle& handle, Wrap wrapping = REPEAT);
+        Texture(const Texture& other) = delete;
         ~Texture();
         
         // Functions
--- src/draft/rendering/texture.cpp
+++ src/draft/rendering/texture.cpp
@@ -1,27 +1,15 @@
+#include "draft/util/file_handle.hpp"
 #define STB_IMAGE_IMPLEMENTATION
 
 #include "draft/rendering/texture.hpp"
 #include "glad/gl.h"
 #include "stb_image.h"
 
+using namespace std;
+
 namespace Draft {
     // Private functions
-    void Texture::load_texture(const std::string& texturePath){
-        // Load texture from file
-        stbi_set_flip_vertically_on_load(true);
-        unsigned char *data = stbi_load(texturePath.c_str(), &size.x, &size.y, &nrChannels, 0);
-        loaded = !(bool)(!data);
-
-        bind();
-        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, size.x, size.y, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
-        glGenerateMipmap(GL_TEXTURE_2D);
-        unbind();
-
-        stbi_image_free(data);
-    }
-    
-    // Constructors
-    Texture::Texture(Wrap wrapping) : reloadable(true) {
+    void Texture::generate_opengl(Wrap wrapping){
         glGenTextures(1, &texId);
         bind();
         glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, wrapping);
@@ -30,44 +18,46 @@ namespace Draft {
         glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
         unbind();
     }
-
-    Texture::Texture(const std::string& texturePath, Wrap wrapping) : Texture(wrapping) {
-        path = texturePath;
-        load_texture(texturePath.c_str());
-    }
-
-    Texture::Texture(const unsigned char* data, int width, int height, int channels, Wrap wrapping) : Texture(wrapping) {
-        // Load raw data
-        reloadable = false;
-        loaded = true;
-        size.x = width;
-        size.y = height;
-        nrChannels = channels;
-
-        bind();
-        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
-        glGenerateMipmap(GL_TEXTURE_2D);
-        unbind();
-    }
-
-    Texture::Texture(const char* start, const char* end, Wrap wrapping) : Texture(wrapping) {
-        // Load raw data
-        reloadable = false;
-
+    
+    void Texture::load_texture(const unsigned char* bytes, size_t length, int colorSpace){
+        // Load texture from file
         stbi_set_flip_vertically_on_load(true);
-        unsigned char *data = stbi_load_from_memory(reinterpret_cast<const unsigned char*>(start), end - start, &size.x, &size.y, &nrChannels, 0);
+        unsigned char *data = stbi_load_from_memory(bytes, length, &size.x, &size.y, &nrChannels, 0);
         loaded = !(bool)(!data);
 
         bind();
-        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, size.x, size.y, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
+        glTexImage2D(GL_TEXTURE_2D, 0, colorSpace, size.x, size.y, 0, colorSpace, GL_UNSIGNED_BYTE, data);
         glGenerateMipmap(GL_TEXTURE_2D);
         unbind();
 
         stbi_image_free(data);
     }
 
-    Texture::~Texture(){
+    void Texture::cleanup(){
         glDeleteTextures(1, &texId);
+    }
+    
+    // Constructors
+    Texture::Texture(Wrap wrapping) : reloadable(false) {
+        generate_opengl(wrapping);
+    }
+
+    Texture::Texture(const char* start, const char* end, Wrap wrapping) : reloadable(false) {
+        // Load raw data
+        generate_opengl(wrapping);
+        load_texture(reinterpret_cast<const unsigned char*>(start), end - start);
+    }
+
+    Texture::Texture(const filesystem::path& texturePath, Wrap wrapping) : Texture({ texturePath, FileHandle::LOCAL }, wrapping) {}
+
+    Texture::Texture(const FileHandle& handle, Wrap wrapping) : reloadable(true), handle(handle) {
+        auto bytes = handle.read_bytes();
+        generate_opengl(wrapping);
+        load_texture(reinterpret_cast<const unsigned char*>(bytes.data()), bytes.size());
+    }
+
+    Texture::~Texture(){
+        cleanup();
     }
 
     // Functions
@@ -83,7 +73,8 @@ namespace Draft {
     void Texture::reload(){
         if(!reloadable) return;
         unbind();
-        glDeleteTextures(1, &texId);
-        load_texture(path);
+
+        auto bytes = handle.read_bytes();
+        load_texture(reinterpret_cast<const unsigned char*>(bytes.data()), bytes.size());
     }
 };
