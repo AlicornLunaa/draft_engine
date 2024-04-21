4e8437f03d737a61510815328998a9c44cb414a9
AlicornLunaa <alicornlunaa@gmail.com>
2024-04-21 17:43:48 -0500

    Added rest of the joints

--- include/draft/phys/joint.hpp
+++ include/draft/phys/joint.hpp
@@ -13,10 +13,6 @@ namespace Draft {
 
     // Base class
     class Joint {
-    private:
-        // Private functions
-        void* get_joint_ptr();
-
     public:
         // Enumerators
         enum class Type { UNKNOWN, DISTANCE, REVOLUTE, PRISMATIC, PULLEY, GEAR, MOUSE, WHEEL, WELD, ROPE, FRICTION, MOTOR };
@@ -31,19 +27,20 @@ namespace Draft {
 
         // Functions
         Type get_type() const;
+        World* get_world();
         RigidBody* get_body_a();
         RigidBody* get_body_b();
-
-        virtual Vector2f get_anchor_a() const = 0;
-        virtual Vector2f get_anchor_b() const = 0;
-
-        virtual Vector2f get_reaction_force(float inv_dt) const = 0;
-        virtual float get_reaction_torque(float inv_dt) const = 0;
-        
+        virtual Vector2f get_world_anchor_a() const;
+        virtual Vector2f get_world_anchor_b() const;
+        virtual Vector2f get_reaction_force(float inv_dt) const;
+        virtual float get_reaction_torque(float inv_dt) const;
         bool is_enabled() const;
         bool get_collide_connected() const;
 
     protected:
+        // Private functions
+        void* get_joint_ptr();
+
         // Constructors
         Joint(World* world, RigidBody* body1, RigidBody* body2, void* jointPtr, Type type);
         
@@ -60,5 +57,237 @@ namespace Draft {
     };
 
     // Distance joint
-    
+    class DistanceJoint : public Joint {
+    public:
+        // Functions
+        Vector2f get_local_anchor_a() const;
+        Vector2f get_local_anchor_b() const;
+
+        float get_length() const;
+        float get_min_length() const;
+        float get_max_length() const;
+        float get_current_length() const;
+        float get_stiffness() const;
+        float get_damping() const;
+
+        void set_length(float length);
+        void set_min_length(float length);
+        void set_max_length(float length);
+        void set_stiffness(float stiffness);
+        void set_damping(float damping);
+
+    protected:
+        // Constructors
+        DistanceJoint(World* world, RigidBody* body1, RigidBody* body2, void* jointPtr);
+    };
+
+    // Friction joint
+    class FrictionJoint : public Joint {
+    public:
+        // Functions
+        Vector2f get_local_anchor_a() const;
+        Vector2f get_local_anchor_b() const;
+
+        float get_max_force() const;
+        float get_max_torque() const;
+
+        void set_max_force(float force);
+        void set_max_torque(float torque);
+
+    protected:
+        // Constructors
+        FrictionJoint(World* world, RigidBody* body1, RigidBody* body2, void* jointPtr);
+    };
+
+    // Gear joint
+    class GearJoint : public Joint {
+    private:
+        // Variables
+        Joint* joint1;
+        Joint* joint2;
+
+    public:
+        // Functions
+        Joint* get_joint_1();
+        Joint* get_joint_2();
+        float get_ratio() const;
+        void set_ratio(float ratio);
+
+    protected:
+        // Constructors
+        GearJoint(World* world, RigidBody* body1, RigidBody* body2, Joint* joint1, Joint* joint2, void* jointPtr);
+    };
+
+    // Motor joint
+    class MotorJoint : public Joint {
+    public:
+        // Functions
+        Vector2f get_linear_offset() const;
+        float get_angular_offset() const;
+        float get_max_force() const;
+        float get_max_torque() const;
+        float get_correction_factor() const;
+
+        void set_linear_offset(const Vector2f& offset);
+        void set_angular_offset(float offset);
+        void set_max_force(float force);
+        void set_max_torque(float torque);
+        void set_correction_factor(float force);
+
+    protected:
+        // Constructors
+        MotorJoint(World* world, RigidBody* body1, RigidBody* body2, void* jointPtr);
+    };
+
+    // Mouse joint
+    class MouseJoint : public Joint {
+    public:
+        // Functions
+        Vector2f get_target() const;
+        float get_max_force() const;
+        float get_stiffness() const;
+        float get_damping() const;
+
+        void set_target(const Vector2f& target) const;
+        void set_max_force(float force);
+        void set_stiffness(float stiffness);
+        void set_damping(float damping);
+
+    protected:
+        // Constructors
+        MouseJoint(World* world, RigidBody* body1, RigidBody* body2, void* jointPtr);
+    };
+
+    // Prismatic joint
+    class PrismaticJoint : public Joint {
+    public:
+        // Functions
+        Vector2f get_local_anchor_a() const;
+        Vector2f get_local_anchor_b() const;
+        Vector2f get_local_axis() const;
+        float get_reference_angle() const;
+        float get_joint_translation() const;
+        float get_joint_speed() const;
+        float get_stiffness() const;
+        float get_damping() const;
+        float get_lower_limit() const;
+        float get_upper_limit() const;
+        float get_motor_speed() const;
+        float get_max_motor_force() const;
+        float get_motor_force(float inv_dt) const;
+        bool is_limit_enabled() const;
+        bool is_motor_enabled() const;
+
+        void set_target(const Vector2f& target) const;
+        void set_max_force(float force);
+        void set_stiffness(float stiffness);
+        void set_damping(float damping);
+        void set_limits(float lower, float upper);
+        void set_motor_speed(float speed);
+        void set_max_motor_force(float force);
+        void enable_limit(bool flag);
+        void enable_motor(bool flag);
+        
+    protected:
+        // Constructors
+        PrismaticJoint(World* world, RigidBody* body1, RigidBody* body2, void* jointPtr);
+    };
+
+    // Pulley joint
+    class PulleyJoint : public Joint {
+    public:
+        // Functions
+        Vector2f get_ground_anchor_a() const;
+        Vector2f get_ground_anchor_b() const;
+        float get_length_a() const;
+        float get_length_b() const;
+        float get_ratio() const;
+        float get_current_length_a() const;
+        float get_current_length_b() const;
+        
+    protected:
+        // Constructors
+        PulleyJoint(World* world, RigidBody* body1, RigidBody* body2, void* jointPtr);
+    };
+
+    // Revolute joint
+    class RevoluteJoint : public Joint {
+    public:
+        // Functions
+        Vector2f get_local_anchor_a() const;
+        Vector2f get_local_anchor_b() const;
+        float get_reference_angle() const;
+        float get_joint_angle() const;
+        float get_joint_speed() const;
+        float get_lower_limit() const;
+        float get_upper_limit() const;
+        float get_motor_speed() const;
+        float get_max_motor_torque() const;
+        float get_motor_torque(float inv_dt) const;
+        bool is_limit_enabled() const;
+        bool is_motor_enabled() const;
+
+        void enable_limit(bool flag);
+        void enable_motor(bool flag);
+        void set_limits(float lower, float upper);
+        void set_motor_speed(float speed);
+        void set_max_motor_torque(float torque);
+        
+    protected:
+        // Constructors
+        RevoluteJoint(World* world, RigidBody* body1, RigidBody* body2, void* jointPtr);
+    };
+
+    // Weld joint
+    class WeldJoint : public Joint {
+    public:
+        // Functions
+        Vector2f get_local_anchor_a() const;
+        Vector2f get_local_anchor_b() const;
+        float get_reference_angle() const;
+        float get_stiffness() const;
+        float get_damping() const;
+
+        void set_stiffness(float hz);
+        void set_damping(float damping);
+        
+    protected:
+        // Constructors
+        WeldJoint(World* world, RigidBody* body1, RigidBody* body2, void* jointPtr);
+    };
+
+
+    // Wheel joint
+    class WheelJoint : public Joint {
+    public:
+        // Functions
+        Vector2f get_local_anchor_a() const;
+        Vector2f get_local_anchor_b() const;
+        Vector2f get_local_axis() const;
+        float get_joint_translation() const;
+        float get_joint_linear_speed() const;
+        float get_joint_angle() const;
+        float get_joint_angular_speed() const;
+        float get_lower_limit() const;
+        float get_upper_limit() const;
+        float get_motor_speed() const;
+        float get_max_motor_torque() const;
+        float get_motor_torque(float inv_dt) const;
+        float get_stiffness() const;
+        float get_damping() const;
+        bool is_limit_enabled() const;
+        bool is_motor_enabled() const;
+
+        void enable_limit(bool flag);
+        void enable_motor(bool flag);
+        void set_limits(float lower, float upper);
+        void set_motor_speed(float speed);
+        void set_max_motor_torque(float torque);
+        void set_stiffness(float stiffness);
+        void set_damping(float damping);
+        
+    protected:
+        // Constructors
+        WheelJoint(World* world, RigidBody* body1, RigidBody* body2, void* jointPtr);
+    };
 };
--- src/draft/phys/joint.cpp
+++ src/draft/phys/joint.cpp
@@ -8,6 +8,7 @@
 #include <memory>
 
 namespace Draft {
+    // Start Base Joint
     // pImpl
     struct Joint::Impl {
         b2Joint* joint;
@@ -28,15 +29,19 @@ namespace Draft {
 
     // Functions
     Joint::Type Joint::get_type() const { return type; };
+    World* Joint::get_world(){ return world; }
     RigidBody* Joint::get_body_a(){ return bodyPtr1; }
     RigidBody* Joint::get_body_b(){ return bodyPtr2; }
 
-    Vector2f Joint::get_anchor_a() const { return b2_to_vector<float>(ptr->joint->GetAnchorA()); }
-    Vector2f Joint::get_anchor_b() const { return b2_to_vector<float>(ptr->joint->GetAnchorB()); }
-
+    Vector2f Joint::get_world_anchor_a() const { return b2_to_vector<float>(ptr->joint->GetAnchorA()); }
+    Vector2f Joint::get_world_anchor_b() const { return b2_to_vector<float>(ptr->joint->GetAnchorB()); }
     Vector2f Joint::get_reaction_force(float inv_dt) const { return b2_to_vector<float>(ptr->joint->GetReactionForce(inv_dt)); };
     float Joint::get_reaction_torque(float inv_dt) const { return ptr->joint->GetReactionTorque(inv_dt); };
     
     bool Joint::is_enabled() const { return ptr->joint->IsEnabled(); }
     bool Joint::get_collide_connected() const { return ptr->joint->GetCollideConnected(); }
+    // End Base Joint
+    
+    // Start Distance Joint
+    // End Distance Joint
 };
