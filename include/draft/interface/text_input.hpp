1c3219aa0ff637dbb351625e219913ad8e7567c0
AlicornLunaa <alicornlunaa@gmail.com>
2024-12-04 14:09:48 -0600

    Fleshed out text input

--- include/draft/interface/text_input.hpp
+++ include/draft/interface/text_input.hpp
@@ -2,24 +2,39 @@
 
 #include "draft/interface/panel.hpp"
 #include "draft/rendering/batching/text_renderer.hpp"
+
+#include <functional>
 #include <string>
+#include <vector>
 
 namespace Draft {
     class TextInput : public Panel {
     private:
+        // Types
+        typedef std::function<void(std::string)> EnterFunc;
+
         // Variables
         TextRenderer textRenderer;
         std::string* str = nullptr;
         bool selected = false;
+        EnterFunc enterFunc; // Function when enter is pressed
 
-        Vector2f textBounds;
+        std::vector<float> charWidth;
+        uint stringStart = 0;
+        uint stringLen = 0;
+        uint cursorPos = 0;
         float animTimer = 0.f;
+
+        // Private functions
+        void cursor_left();
+        void cursor_right();
 
     public:
         // Constructor
         TextInput(float x, float y, float w, float h, std::string* str, Panel* parent = nullptr);
 
         // Functions
+        inline void set_enter_func(EnterFunc f){ enterFunc = f; }
         virtual bool handle_event(const Event& event) override;
         virtual void paint(const Time& deltaTime, SpriteBatch& batch) override;
     };
--- src/draft/interface/text_input.cpp
+++ src/draft/interface/text_input.cpp
@@ -4,33 +4,104 @@
 #include "draft/rendering/batching/text_renderer.hpp"
 
 namespace Draft {
+    // Private functions
+    void TextInput::cursor_left(){
+        // Moves the cursor left
+        cursorPos--;
+
+        if(cursorPos <= stringStart && stringStart > 0){
+            stringStart--;
+        }
+    }
+
+    void TextInput::cursor_right(){
+        // Moves the cursor right
+        cursorPos++;
+
+        if(cursorPos > stringStart + stringLen && stringStart < stringLen){
+            stringStart++;
+        }
+    }
+
     // Constructor
     TextInput::TextInput(float x, float y, float w, float h, std::string* str, Panel* parent) : Panel(parent), str(str) {
         bounds.x = x;
         bounds.y = y;
         bounds.width = w;
         bounds.height = h;
+
+        *str = "";
     }
 
     // Functions
     bool TextInput::handle_event(const Event& event){
         switch(event.type){
-        case Event::MouseButtonPressed:
+        case Event::MouseButtonReleased:
             selected = Math::contains(bounds, {event.mouseButton.x, event.mouseButton.y});
             return selected;
 
+        case Event::KeyHold:
         case Event::KeyPressed:
-            if(event.key.code == Keyboard::BACKSPACE){ str->erase(str->begin() + str->size() - 1); }
-            // if(event.key.code == Keyboard::DELETE){ str->erase(str->begin() + str->size()); }
+            // Handle all text controls
+            if(selected && str->length() > 0){
+                if(event.key.code == Keyboard::BACKSPACE && cursorPos > 0){
+                    // Backspace removes the character behind the cursor
+                    str->erase(str->begin() + cursorPos - 1);
+                    charWidth.erase(charWidth.begin() + cursorPos);
+                    cursor_left();
+                } else if(event.key.code == Keyboard::DELETE && cursorPos < str->length()){
+                    // Delete removes the character in front of the cursor
+                    str->erase(str->begin() + cursorPos);
+                    charWidth.erase(charWidth.begin() + cursorPos);
+                } else if(event.key.code == Keyboard::LEFT && cursorPos > 0){
+                    // Shifts the cursor left
+                    cursor_left();
+                } else if(event.key.code == Keyboard::RIGHT && cursorPos < str->length()){
+                    // Shifts the cursor right
+                    cursor_right();
+                } else if(event.key.code == Keyboard::HOME){
+                    // Set cursor to the beginning
+                    cursorPos = 0;
+                    stringStart = 0;
+                } else if(event.key.code == Keyboard::END){
+                    // Sets cursor to the end
+                    cursorPos = str->length();
+                    stringStart = std::max((uint)0, (uint)str->length() - stringLen);
+                } else if(event.key.code == Keyboard::ESCAPE){
+                    // Unselect the bar
+                    selected = false;
+                } else if(event.key.code == Keyboard::ENTER){
+                    // Sets cursor to the end
+                    selected = false;
+
+                    if(enterFunc)
+                        enterFunc(*str);
+                }
+            }
             break;
 
         case Event::TextEntered:
+            // Handle text inputs
             if(selected){
-                *str += (char)event.text.unicode;
+                // Use this to get the width of the string
+                TextProperties props{
+                    std::string(1, (char)event.text.unicode),
+                    nullptr,
+                    {bounds.x + 10, bounds.y + bounds.height * 0.5f},
+                    {0, 0.5f},
+                    {1, 1, 1, 1},
+                    0.f,
+                    1.f
+                };
+
+                (*str).insert(cursorPos, props.str);
+                charWidth.insert(charWidth.begin() + cursorPos, textRenderer.get_text_bounds(props).x);
+                stringLen = Math::min((float)str->length(), bounds.width / 16.f - 1);
+                cursor_right();
+
                 return true;
             }
-            
-            return false;
+            break;
 
         default:
             break;
@@ -40,9 +111,9 @@ namespace Draft {
     }
     
     void TextInput::paint(const Time& deltaTime, SpriteBatch& batch){
-        // Get info for rendering correctly
+        // Get info for rendering correctly. Render from the start of the string
         TextProperties props{
-            *str,
+            (*str).substr(stringStart, stringLen),
             nullptr,
             {bounds.x + 10, bounds.y + bounds.height * 0.5f},
             {0, 0.5f},
@@ -50,7 +121,6 @@ namespace Draft {
             0.f,
             1.f
         };
-        textBounds = textRenderer.get_text_bounds(props);
 
         // Basic rectangle which changes color based on its value
         if(selected){
@@ -58,10 +128,15 @@ namespace Draft {
             animTimer += deltaTime.as_seconds() * 2.f;
 
             if((int)animTimer % 2 == 0){
+                float cursorPixelPos = 0.f;
+
+                for(uint c = stringStart; c < cursorPos; c++)
+                    cursorPixelPos += charWidth[c];
+
                 batch.draw({
                     nullptr,
                     {},
-                    {bounds.x + textBounds.x + 4, bounds.y + bounds.height * 0.5f},
+                    {bounds.x + cursorPixelPos + 10, bounds.y + bounds.height * 0.5f},
                     0.f,
                     {6, bounds.height * 0.75f},
                     {0, bounds.height * 0.75f * 0.5f},
