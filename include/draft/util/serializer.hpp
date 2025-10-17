f890844e2164f7666d3ca6feb5d166159aad6d65
AlicornLunaa <alicornlunaa@gmail.com>
2025-10-17 12:19:15 -0500

    Added binary vector serializer

--- include/draft/util/serializer.hpp
+++ include/draft/util/serializer.hpp
@@ -1,13 +1,18 @@
 #pragma once
 
 #include <bit>
+#include <cstddef>
+#include <vector>
 #include "draft/util/json.hpp"
 
 // Main declarations here
 namespace Draft {
     namespace Binary {
-        // Build variables
+        // Build variables and types
         inline constexpr bool IS_LITTLE_ENDIAN = std::endian::native == std::endian::little;
+        typedef std::vector<std::byte> ByteArray;
+        typedef std::span<std::byte> ByteSpan;
+        typedef std::span<const std::byte> ConstByteSpan;
     
         // Helpers
         template<typename T>
@@ -38,7 +43,7 @@ namespace Draft {
 
         // Binary operations
         template<typename T>
-        inline void write(std::vector<std::byte>& buffer, const T& value){
+        inline void write(ByteArray& buffer, const T& value){
             // Simply writes the buffer with the value
             static_assert(std::is_trivially_copyable_v<T>, "T must be trivially copyable");
             T littleEndian = to_little_endian(value);
@@ -47,7 +52,7 @@ namespace Draft {
         }
 
         template<typename T>
-        inline void read(std::span<const std::byte> span, T& value){
+        inline void read(ConstByteSpan span, T& value){
             // Simply assigns the value with the data from the span.
             static_assert(std::is_trivially_copyable_v<T>, "T must be trivially copyable");
 
@@ -59,7 +64,7 @@ namespace Draft {
         }
         
         template<typename T>
-        inline void read_and_advance(std::span<const std::byte>& span, T& value){
+        inline void read_and_advance(ConstByteSpan& span, T& value){
             // Simply assigns the value with the data from the span.
             read(span, value);
 
@@ -71,7 +76,7 @@ namespace Draft {
     namespace Serializer {
         // Serializable types
         template<typename T>
-        concept Serializable = requires(T object, std::vector<std::byte>& buffer, std::span<const std::byte>& span, Draft::JSON& json){
+        concept Serializable = requires(T object, Binary::ByteArray& buffer, Binary::ConstByteSpan& span, Draft::JSON& json){
             { T::serialize(object, buffer) } -> std::convertible_to<void>;
             { T::deserialize(object, span) } -> std::convertible_to<void>;
             { T::serialize(object, json) } -> std::convertible_to<void>;
@@ -80,10 +85,16 @@ namespace Draft {
 
         // Default for complex types
         template<Serializable T>
-        inline void serialize(const T& value, std::vector<std::byte>& out){ T::serialize(value, out); }
+        inline void serialize(const T& value, Binary::ByteArray& out){ T::serialize(value, out); }
 
         template<Serializable T>
-        inline void deserialize(T& value, std::span<const std::byte> span){ T::deserialize(span, value); }
+        inline void deserialize(T& value, Binary::ConstByteSpan span){ T::deserialize(span, value); }
+
+        template<Serializable T>
+        inline void deserialize_and_advance(T& value, Binary::ConstByteSpan& span){
+            T::deserialize(value, span);
+            span = span.subspan(sizeof(T));
+        }
 
         // Default for JSON-serializable complex types
         template<Serializable T>
@@ -92,15 +103,19 @@ namespace Draft {
         template<Serializable T>
         inline void deserialize(T& value, JSON& json){ T::deserialize(value, json); }
 
+
         // Default for trivial types
         template<typename T> requires std::is_trivially_copyable_v<T>
-        inline void serialize(const T& value, std::vector<std::byte>& out){ Binary::write(out, value); }
+        inline void serialize(const T& value, Binary::ByteArray& out){ Binary::write(out, value); }
 
         template<typename T> requires std::is_trivially_copyable_v<T>
-        inline void deserialize(T& value, std::span<const std::byte> span){ Binary::read(span, value); }
+        inline void deserialize(T& value, Binary::ConstByteSpan span){ Binary::read(span, value); }
 
-        template<typename T>
-        inline void deserialize_and_advance(T& value, std::span<const std::byte>& span){ Binary::read_and_advance(span, value); }
+        template<typename T> requires std::is_trivially_copyable_v<T>
+        inline void deserialize_and_advance(T& value, Binary::ConstByteSpan& span){
+            deserialize(value, span);
+            span = span.subspan(sizeof(T));
+        }
 
         // Default for JSON-serializable trivials
         template<typename T> requires std::is_arithmetic_v<T> || std::is_enum_v<T> || std::is_same_v<T, std::string>
@@ -108,5 +123,32 @@ namespace Draft {
 
         template<typename T> requires std::is_arithmetic_v<T> || std::is_enum_v<T> || std::is_same_v<T, std::string>
         inline void deserialize(T& value, JSON& json){ value = json.template get<T>(); }
+
+
+        // Basic included non-trivial serializers
+        template<typename K>
+        inline void serialize(const std::vector<K>& array, Binary::ByteArray& out){
+            // Serialize a vector array, starting with the size
+            serialize(array.size(), out);
+            
+            for(auto& val : array){
+                serialize(val, out);
+            }
+        }
+
+        template<typename K>
+        inline void deserialize(std::vector<K>& array, Binary::ConstByteSpan span){
+            // Serialize a vector array, starting with the size
+            size_t size = 0;
+            deserialize_and_advance(size, span);
+
+            array.reserve(size);
+            
+            for(size_t i = 0; i < size; i++){
+                K value{};
+                deserialize_and_advance(value, span);
+                array.push_back(value);
+            }
+        }
     };
 };
