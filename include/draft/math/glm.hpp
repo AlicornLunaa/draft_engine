a38fcbed23697889988a5013c4fa69f7c3a57319
AlicornLunaa <alicornlunaa@gmail.com>
2025-12-06 22:08:05 -0600

    Updated GLM

--- include/draft/math/glm.hpp
+++ include/draft/math/glm.hpp
@@ -7,6 +7,7 @@
 #include "glm/ext/matrix_clip_space.hpp" // IWYU pragma: keep, Includes definitions and such
 #include "glm/gtx/rotate_vector.hpp" // IWYU pragma: keep, Includes definitions and such
 #include "glm/geometric.hpp" // IWYU pragma: keep, Includes definitions and such
+#include "glm/gtx/norm.hpp"
 
 typedef unsigned int uint;
 
