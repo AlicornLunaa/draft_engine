238d3a5e12b6c2104b410e89d2d8aaf3fe45f7a0
AlicornLunaa <alicornlunaa@gmail.com>
2026-07-12 03:22:18 -0500

    Added TextureComponent

---
+++ runtime/include/draft/components/texture_component.hpp
@@ -0,0 +1,32 @@
+#pragma once
+
+#include "draft/rendering/texture.hpp"
+#include "draft/asset/resource.hpp"
+
+#include <vector>
+
+namespace Draft {
+    /**
+     * @brief Contains list of texture pointers
+     */
+    struct TextureComponent {
+        // Variables
+        std::vector<Resource<Texture>> textures{};
+
+        // Constructors
+        TextureComponent(const TextureComponent& transform) = default;
+
+        template<std::same_as<Resource<Texture>> ...Args>
+        TextureComponent(Args... args) : textures(args...) {}
+
+        // Functions
+        /**
+         * @brief Binds every texture pointer supplies in order of the spots they were in for the component
+         */
+        void bind(){
+            for(size_t i = 0; i < textures.size(); i++){
+                textures[i]->bind(i);
+            }
+        }
+    };
+}
